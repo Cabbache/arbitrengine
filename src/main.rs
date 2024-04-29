e9d2781a995adaf529db9ad016205d7613998894
Cabbache <cabbache@cabbache.net>
2024-04-29 02:05:40 +0200

    list negative cycles

--- src/main.rs
+++ src/main.rs
@@ -36,35 +36,44 @@ fn parse_input(data: String) -> Result<Vec<Exchange>, String> {
 		}
 	})
 	.collect()
+	//TODO make sure pairs are unique
+}
+
+fn asset_to_index(
+	asset_name: String,
+	currency_index: &mut IndexMap,
+	currencies: &mut CGraph
+) -> NodeIndex {
+	match currency_index.get(&asset_name) {
+		Some(index) => *index,
+		None => {
+			let index = currencies.add_node(asset_name.clone());
+			currency_index.insert(asset_name, index);
+			index
+		}
+	}
 }
 
 fn update_states(
 	currencies: &mut CGraph, 
-	currency_index: &IndexMap,
+	currency_index: &mut IndexMap,
 	data: &Vec<Exchange>
 ) {
-	
+	for item in data {
+		let from_index = asset_to_index(item.from.clone(), currency_index, currencies);
+		let to_index = asset_to_index(item.to.clone(), currency_index, currencies);
+
+		currencies.update_edge(from_index, to_index, item.rate);
+		currencies.update_edge(to_index, from_index, 1f64/item.rate);
+	}
 }
 
+/*
+5/6/1,5/7/1,5/8/1,6/8/1,7/6/1,8/7/-1,0/1/1,0/2/1,0/3/1,1/3/1,2/1/1,3/2/-1
+*/
 fn main() {
-//	let graph_with_neg_cycle = Graph::<(), f32, Directed>::from_edges(&[
-//		(0, 1, 1.),
-//		(0, 2, 1.),
-//		(0, 3, 1.),
-//		(1, 3, 1.),
-//		(2, 1, 1.),
-//		(3, 2, -3.),
-//	]);
-//	let path = find_negative_cycle(&graph_with_neg_cycle, NodeIndex::new(0));
-//	println!("{:?}", path);
-
-	let currency_index = IndexMap::new();
+	let mut currency_index = IndexMap::new();
 	let mut currencies = CGraph::new();
-
-	let index1 = currencies.add_node("USD".to_string());
-	let index2 = currencies.add_node("EUR".to_string());
-	println!("{:?}", index1);
-	println!("{:?}", index2);
 
 	loop {
 		let mut updates = String::new();
@@ -73,8 +82,16 @@ fn main() {
 		let input_data = parse_input(updates);
 		match input_data {
 			Ok(data) => {
-				println!("{:?}", data);
-				update_states(&mut currencies, &currency_index, &data);
+				update_states(&mut currencies, &mut currency_index, &data);
+				let neg_cycles = currencies
+					.node_indices()
+					.filter_map(
+						|index| find_negative_cycle(&currencies, index)
+							.and_then(|negc| Some((index, negc)))
+					);
+				for cc in neg_cycles {
+					println!("{:?}", cc);
+				}
 			},
 			Err(msg) => eprintln!("{}", msg)
 		}
