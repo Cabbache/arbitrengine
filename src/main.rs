220a2b9f943d8b921af07c665f7a41cf13611632
Cabbache <cabbache@cabbache.net>
2024-05-11 22:28:46 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::ops::Add;
 
 use std::collections::{HashMap, HashSet};
 
-use petgraph::Graph;
 use petgraph::algo::find_negative_cycle;
-use petgraph::prelude::*;
 use petgraph::algo::FloatMeasure;
+use petgraph::prelude::*;
+use petgraph::Graph;
 
-type CGraph = Graph::<String, Weight, Directed>;
-type IndexMap = HashMap::<String, NodeIndex>;
+type CGraph = Graph<String, Weight, Directed>;
+type IndexMap = HashMap<String, NodeIndex>;
 type GraphPath = Vec<NodeIndex>;
 
 #[derive(Clone, Debug)]
@@ -34,8 +34,8 @@ impl FloatMeasure for Weight {
 
 	fn infinite() -> Self {
 		Self {
-			weight: 1./0.,
-			rate: 1./0.,
+			weight: 1. / 0.,
+			rate: 1. / 0.,
 		}
 	}
 }
@@ -55,86 +55,93 @@ fn mkerror(msg: String) -> String {
 
 fn parse_input(data: String) -> Result<Vec<Exchange>, String> {
 	data.split(",")
-	.map(|value| {
-		let exchstr = value.split("/").collect::<Vec<&str>>();
-		match exchstr[..] {
-			[from, to, ratestr] => {
-				let rate = str::parse::<f64>(ratestr);
-				if rate.is_err() {
-					return Err(format!("'{}' not a float", ratestr));
+		.map(|value| {
+			let exchstr = value.split("/").collect::<Vec<&str>>();
+			match exchstr[..] {
+				[from, to, ratestr] => {
+					let rate = str::parse::<f64>(ratestr);
+					if rate.is_err() {
+						return Err(format!("'{}' not a float", ratestr));
+					}
+					Ok(Exchange {
+						from: from.to_string(),
+						to: to.to_string(),
+						rate: rate.unwrap(),
+					})
 				}
-				Ok(Exchange {
-					from: from.to_string(),
-					to: to.to_string(),
-					rate: rate.unwrap(),
-				})
+				_ => Err(format!("bad format '{}'", value).to_string()),
 			}
-			_ => Err(format!("bad format '{}'", value).to_string())
-		}
-	})
-	.collect::<Result<Vec<Exchange>, String>>()
-	.and_then(|list| {
-		let unique_len = HashSet::<(String, String)>::from_iter(
-			list.clone()
-			.into_iter()
-			.map(|exchange| (exchange.to, exchange.from))
-		).len();
-		match unique_len == list.len() { 
-			true => Ok(list),
-			false => Err("duplicate entries".to_string())
-		}
-	})
+		})
+		.collect::<Result<Vec<Exchange>, String>>()
+		.and_then(|list| {
+			let unique_len = HashSet::<(String, String)>::from_iter(
+				list.clone()
+					.into_iter()
+					.map(|exchange| (exchange.to, exchange.from)),
+			)
+			.len();
+			match unique_len == list.len() {
+				true => Ok(list),
+				false => Err("duplicate entries".to_string()),
+			}
+		})
 }
 
 fn path_to_string(path: GraphPath, currencies: &CGraph) -> String {
 	let indexes = path.iter().map(|index| currencies[*index].clone());
 
-	indexes.clone().zip(indexes.skip(1))
-	.map(|(a, b)| format!("{}/{}", a, b))
-	.collect::<Vec<String>>()
-	.join(",")
+	indexes
+		.clone()
+		.zip(indexes.skip(1))
+		.map(|(a, b)| format!("{}/{}", a, b))
+		.collect::<Vec<String>>()
+		.join(",")
 }
 
 fn get_neg_cycles(currencies: &CGraph) -> Vec<(GraphPath, f64)> {
 	currencies
-	.node_indices()
-	.filter_map(
-		|index| find_negative_cycle(&currencies, index)
-		.and_then(|mut negc| {
-			println!("{:?} {:?}", index, negc);
-			if *negc.last().unwrap() != index {
-				negc.push(index);
-			}
-			if *negc.first().unwrap() != index {
-				let mut cl = vec![index];
-				cl.extend(negc);
-				negc = cl;
-			}
-			if negc.len() < 2 {
-				return None
-			}
+		.node_indices()
+		.filter_map(|index| {
+			find_negative_cycle(&currencies, index).and_then(|mut negc| {
+				println!("{:?} {:?}", index, negc);
+				if *negc.last().unwrap() != index {
+					negc.push(index);
+				}
+				if *negc.first().unwrap() != index {
+					let mut cl = vec![index];
+					cl.extend(negc);
+					negc = cl;
+				}
+				if negc.len() < 2 {
+					return None;
+				}
 
-			let conversion = negc.iter()
-			.zip(negc.iter().skip(1))
-			.map(|(index1, index2)| {
-				println!("{:?} {:?}", currencies[*index1], currencies[*index2]);
-				let edge_value = currencies[currencies.find_edge(*index1, *index2).unwrap()];
-				edge_value
-			}).fold(1f64, |acc, weight| acc*weight.rate);
+				let conversion = negc
+					.iter()
+					.zip(negc.iter().skip(1))
+					.map(|(index1, index2)| {
+						println!("{:?} {:?}", currencies[*index1], currencies[*index2]);
+						let edge_value =
+							currencies[currencies.find_edge(*index1, *index2).unwrap()];
+						edge_value
+					})
+					.fold(1f64, |acc, weight| acc * weight.rate);
 
-			if conversion <= 1f64 { //It's not profitable
-				return None
-			}
+				if conversion <= 1f64 {
+					//It's not profitable
+					return None;
+				}
 
-			Some((negc, conversion))
+				Some((negc, conversion))
+			})
 		})
-	).collect::<Vec<(GraphPath, f64)>>()
+		.collect::<Vec<(GraphPath, f64)>>()
 }
 
 fn asset_to_index(
 	asset_name: String,
 	currency_index: &mut IndexMap,
-	currencies: &mut CGraph
+	currencies: &mut CGraph,
 ) -> NodeIndex {
 	match currency_index.get(&asset_name) {
 		Some(index) => *index,
@@ -146,23 +153,27 @@ fn asset_to_index(
 	}
 }
 
-fn update_states(
-	currencies: &mut CGraph, 
-	currency_index: &mut IndexMap,
-	data: &Vec<Exchange>
-) {
+fn update_states(currencies: &mut CGraph, currency_index: &mut IndexMap, data: &Vec<Exchange>) {
 	for item in data {
 		let from_index = asset_to_index(item.from.clone(), currency_index, currencies);
 		let to_index = asset_to_index(item.to.clone(), currency_index, currencies);
 
-		currencies.update_edge(from_index, to_index, Weight {
-			weight: -item.rate.log2(),
-			rate: item.rate,
-		});
-		currencies.update_edge(to_index, from_index, Weight {
-			weight: -(1f64/item.rate).log2(),
-			rate: 1f64/item.rate,
-		});
+		currencies.update_edge(
+			from_index,
+			to_index,
+			Weight {
+				weight: -item.rate.log2(),
+				rate: item.rate,
+			},
+		);
+		currencies.update_edge(
+			to_index,
+			from_index,
+			Weight {
+				weight: -(1f64 / item.rate).log2(),
+				rate: 1f64 / item.rate,
+			},
+		);
 	}
 }
 
@@ -185,8 +196,8 @@ fn main() {
 				for cycle in get_neg_cycles(&currencies) {
 					println!("CYCLE {} {}", path_to_string(cycle.0, &currencies), cycle.1);
 				}
-			},
-			Err(msg) => eprintln!("{}", mkerror(msg))
+			}
+			Err(msg) => eprintln!("{}", mkerror(msg)),
 		}
 	}
 }
